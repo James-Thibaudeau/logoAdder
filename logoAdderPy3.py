0d40315405734caeb4bfec474d8f9ed2d765285e
Allen Hsu <allenhsu@Allens-MacBook-Pro.local>
2016-04-11 14:40:25 -0400

    Updated logoAdder

--- logoAdderPy3.py
+++ logoAdderPy3.py
@@ -12,6 +12,7 @@ print("This is James' logo adding tool version 1.0 PYTHON 3")
 print("\n**************************\n")
 
 LOGO_FILENAME = input("Enter a file name: ")
+ratio = float(input("Enter the ratio of the logo to the image: "))
 
 logoIm = Image.open(LOGO_FILENAME)
 logoWidth, logoHeight = logoIm.size
@@ -20,13 +21,11 @@ if not os.path.exists('withLogo'):
     os.makedirs('withLogo')
 
 # change this ratio variable according to the width of the image
-ratio = 4
-aspRatio = 0.3804262036
+aspRatio = float(logoWidth / logoHeight)
 
 #Timer start
 start = time.clock()
 count = 0
-
 #Loop over all photos in a directory
 for filename in os.listdir('.'):
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
@@ -34,9 +33,9 @@ for filename in os.listdir('.'):
 	    continue
 		
     im = Image.open(filename) 
-	imWidth, imHeight = im.size
-    logoWidth = int(imWidth / ratio)
-    logoHeight = int(logoWidth * aspRatio)
+    imWidth, imHeight = im.size
+    logoWidth = int(imWidth * ratio)
+    logoHeight = int(logoWidth / aspRatio)
     logoIm = logoIm.resize((logoWidth, logoHeight), Image.ANTIALIAS)
 
     print('Adding %s to %s...' %(LOGO_FILENAME, filename))
